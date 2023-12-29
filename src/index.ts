69b6509a433a50ed645a7c99969d49686555cfc8
Guilherme Aranega <guilhermee.aranega@gmail.com>
2023-12-29 20:41:10 -0300

    started to make the user domain

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import app from "./app";
+import "dotenv/config";
+
+const port = process.env.PORT;
+
+const startServer = async () => {
+	try {
+		await app.listen(port);
+		console.log(`Server running on port ${port}`);
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+startServer();
