13b74f95b7b96a88f6ab4ed0af3c3676f3fb81bc
Guilherme Aranega <guilhermee.aranega@gmail.com>
2023-12-30 16:23:51 -0300

    added the appointment route on the root route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,10 +5,12 @@ const router = express.Router();
 import customerRoutes from '../domains/customer/routes';
 import barberRoutes from '../domains/barber/routes';
 import haircutTypeRoutes from '../domains/haircutType/routes';
+import appointmentRoutes from '../domains/appointment/routes';
 
 router.use('/customer', customerRoutes);
 router.use('/barber', barberRoutes);
 router.use('/haircutType', haircutTypeRoutes);
+router.use('/appointment', appointmentRoutes);
 
 router.get('/', (req, res) => {
 	res.send('Server running');
