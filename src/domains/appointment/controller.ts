36279cc5101b3a7181eacd55194586b0a2c72172
Guilherme Aranega <guilhermee.aranega@gmail.com>
2023-12-30 16:22:15 -0300

    removed cascade on delete a appointment

---
+++ src/domains/appointment/controller.ts
@@ -0,0 +1,160 @@
+import appointment from '../../db/schema/appointment.schema';
+import db from '../../db/db';
+import { eq, or } from 'drizzle-orm';
+
+type IAppointment = typeof appointment.$inferInsert;
+
+// Create a new appointment
+const createNewAppointment = async (data: IAppointment) => {
+	try {
+		const { date, time, customer_id, barber_id, type_id } = data;
+
+		if (!date || !time || !customer_id || !barber_id || !type_id) {
+			throw new Error('All the fields are required');
+		}
+
+		const existingAppointment = await db
+			.select()
+			.from(appointment)
+			.where(or(eq(appointment.date, date), eq(appointment.time, time)));
+
+		if (existingAppointment.length > 0) {
+			throw new Error('Appointment already exists');
+		}
+
+		const newAppointment = await db
+			.insert(appointment)
+			.values({
+				...data,
+			})
+			.returning();
+
+		return newAppointment;
+	} catch (error) {
+		throw error;
+	}
+};
+
+// Get all appointments (limit 10)
+const getAppointments = async () => {
+	try {
+		const appointments = await db.select().from(appointment).limit(10);
+
+		return appointments;
+	} catch (error) {
+		throw error;
+	}
+};
+
+// Get appointments by date
+const getAppointmentByDate = async (date: string) => {
+	try {
+		if (!date) {
+			throw new Error('Date is required');
+		}
+		const appointmentByDate = await db
+			.select()
+			.from(appointment)
+			.where(eq(appointment.date, date));
+
+		return appointmentByDate;
+	} catch (error) {
+		throw error;
+	}
+};
+
+// Delete an appointment
+const deleteAppointment = async (id: string) => {
+	try {
+		const deletedAppointment = await db
+			.delete()
+			.from(appointment)
+			.where(eq(appointment.id, id));
+
+		return deletedAppointment;
+	} catch (error) {
+		throw error;
+	}
+};
+
+// Update an appointment
+const updateAppointment = async (id: string, data: IAppointment) => {
+	try {
+		if (!id) {
+			throw new Error('Id is required');
+		}
+
+		const updatedAppointment = await db
+			.update(appointment)
+			.set({
+				...data,
+			})
+			.where(eq(appointment.id, id));
+
+		return updatedAppointment;
+	} catch (error) {
+		throw error;
+	}
+};
+
+// Get appointments by barber id
+const getAppointmentsByBarberId = async (barber_id: string) => {
+	try {
+		if (!barber_id) {
+			throw new Error('Barber id is required');
+		}
+		const appointmentsByBarberId = await db
+			.select()
+			.from(appointment)
+			.where(eq(appointment.barber_id, barber_id));
+
+		return appointmentsByBarberId;
+	} catch (error) {
+		throw error;
+	}
+};
+
+// Get appointments by customer id
+const getAppointmentsByCustomerId = async (customer_id: string) => {
+	try {
+		if (!customer_id) {
+			throw new Error('Customer id is required');
+		}
+		const appointmentsByCustomerId = await db
+			.select()
+			.from(appointment)
+			.where(eq(appointment.customer_id, customer_id));
+
+		return appointmentsByCustomerId;
+	} catch (error) {
+		throw error;
+	}
+};
+
+// Get appointments by type id
+const getAppointmentsByTypeId = async (type_id: string) => {
+	try {
+		if (!type_id) {
+			throw new Error('Type id is required');
+		}
+		const appointmentsByTypeId = await db
+			.select()
+			.from(appointment)
+			.where(eq(appointment.type_id, type_id));
+
+		return appointmentsByTypeId;
+	} catch (error) {
+		throw error;
+	}
+};
+
+export {
+	createNewAppointment,
+	getAppointments,
+	getAppointmentByDate,
+	deleteAppointment,
+	updateAppointment,
+	getAppointmentsByBarberId,
+	getAppointmentsByCustomerId,
+	getAppointmentsByTypeId,
+};
---
+++ src/domains/appointment/routes.ts
@@ -0,0 +1,131 @@
+import express from 'express';
+import {
+	createNewAppointment,
+	deleteAppointment,
+	getAppointmentByDate,
+	getAppointments,
+	getAppointmentsByBarberId,
+	getAppointmentsByCustomerId,
+	getAppointmentsByTypeId,
+	updateAppointment,
+} from './controller';
+
+const router = express.Router();
+
+// Create a new appointment
+router.post('/', async (req, res) => {
+	try {
+		const { date, time, customer_id, barber_id, type_id } = req.body;
+
+		const newAppointment = await createNewAppointment({
+			date,
+			time,
+			customer_id,
+			barber_id,
+			type_id,
+		});
+
+		res.status(201).json(newAppointment);
+	} catch (error) {
+		res.status(500).json({ error });
+	}
+});
+
+// Get all appointments (limit 10)
+router.get('/', async (req, res) => {
+	try {
+		const appointments = await getAppointments();
+
+		res.status(200).json(appointments);
+	} catch (error) {
+		res.status(500).json({ error });
+	}
+});
+
+// Get appointment by date
+router.get('/:date', async (req, res) => {
+	try {
+		const { date } = req.params;
+
+		const appointmentByDate = await getAppointmentByDate(date);
+
+		res.status(200).json(appointmentByDate);
+	} catch (error) {
+		res.status(500).json({ error });
+	}
+});
+
+// Delete appointment by id
+router.delete('/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+
+		const deletedAppointment = await deleteAppointment(id);
+
+		res.status(200).json(deletedAppointment);
+	} catch (error) {
+		res.status(500).json({ error });
+	}
+});
+
+// Update appointment by id
+router.put('/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const { date, time, customer_id, barber_id, type_id } = req.body;
+
+		const updatedAppointment = await updateAppointment(id, {
+			date,
+			time,
+			customer_id,
+			barber_id,
+			type_id,
+		});
+
+		res.status(200).json(updatedAppointment);
+	} catch (error) {
+		res.status(500).json({ error });
+	}
+});
+
+// Get appointments by barber id
+router.get('/barber/:barber_id', async (req, res) => {
+	try {
+		const { barber_id } = req.params;
+
+		const appointmentsByBarberId = await getAppointmentsByBarberId(barber_id);
+
+		res.status(200).json(appointmentsByBarberId);
+	} catch (error) {
+		res.status(500).json({ error });
+	}
+});
+
+// Get appointments by customer id
+router.get('/customer/:customer_id', async (req, res) => {
+	try {
+		const { customer_id } = req.params;
+
+		const appointmentsByCustomerId =
+			await getAppointmentsByCustomerId(customer_id);
+
+		res.status(200).json(appointmentsByCustomerId);
+	} catch (error) {
+		res.status(500).json({ error });
+	}
+});
+
+// Get appointments by type id
+router.get('/type/:type_id', async (req, res) => {
+	try {
+		const { type_id } = req.params;
+
+		const appointmentsByTypeId = await getAppointmentsByTypeId(type_id);
+
+		res.status(200).json(appointmentsByTypeId);
+	} catch (error) {
+		res.status(500).json({ error });
+	}
+});
+
+export default router;
