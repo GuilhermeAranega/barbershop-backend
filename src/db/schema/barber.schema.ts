90492431201c44da7a613be36881b358fea7262d
Guilherme Amo Dauksys Aranega <guilhermee.aranega@gmail.com>
2023-12-30 00:41:10 -0300

    created the barber schema

---
+++ src/db/schema/barber.schema.ts
@@ -0,0 +1,11 @@
+import { createId } from '@paralleldrive/cuid2';
+import { pgTable, varchar, text } from 'drizzle-orm/pg-core';
+
+export const barber = pgTable('barber', {
+    id: text('id').$defaultFn(() => createId()).primaryKey(),
+	firstName: varchar('first_name', { length: 50 }),
+	lastName: varchar('last_name', { length: 50 }),
+	phone: varchar('phone', { length: 50 }),
+});
+
+export default barber;
